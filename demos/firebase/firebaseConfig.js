a3efc04bc267ffbf2511c6e435723f6118888ead
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2022-01-28 23:56:25 +0530

    first commit

---
+++ demos/firebase/firebaseConfig.js
@@ -0,0 +1,34 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB4fWO7nAKX-CuXEyRvK5Tz-m7R8RixByg",
+  authDomain: "clubhouse-94df1.firebaseapp.com",
+  projectId: "clubhouse-94df1",
+  storageBucket: "clubhouse-94df1.appspot.com",
+  messagingSenderId: "290936203144",
+  appId: "1:290936203144:web:f3db9e50d83c8fd8533c3d",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+
+// <script type="module">
+//   // Import the functions you need from the SDKs you need
+//   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js";
+//   // TODO: Add SDKs for Firebase products that you want to use
+//   // https://firebase.google.com/docs/web/setup#available-libraries
+
+//   // Your web app's Firebase configuration
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyB4fWO7nAKX-CuXEyRvK5Tz-m7R8RixByg",
+//     authDomain: "clubhouse-94df1.firebaseapp.com",
+//     projectId: "clubhouse-94df1",
+//     storageBucket: "clubhouse-94df1.appspot.com",
+//     messagingSenderId: "290936203144",
+//     appId: "1:290936203144:web:f3db9e50d83c8fd8533c3d"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+// </script>
